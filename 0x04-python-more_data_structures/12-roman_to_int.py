b6638baedcdea2203acd57cdb60ef960392941e3
gotarazo <2272@holbertonschool.com>
2020-12-18 04:30:45 +0000

    Python more data structures converts roman to int commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or roman_string is None):
         return (0)
-    dic_num = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
+    dic_num = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     n = 0
     for i in range(len(roman_string)):
         if dic_num.get(roman_string[i], 0) == 0:
