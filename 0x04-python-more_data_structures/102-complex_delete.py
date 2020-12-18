b42de6cedbc7043bb1db444afee154591bf28ebe
gotarazo <2272@holbertonschool.com>
2020-12-18 05:00:58 +0000

    Python more data structures deletee keys commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     if a_dictionary is not None and type(a_dictionary) is dict:
-        item = a_dictionary.item()
-        delet = {key: val for (key, val) in item if value == val}
-        for (key, val) in delet.item():
+        items = a_dictionary.items()
+        delet = {key: val for (key, val) in items if value == val}
+        for (key, val) in delet.items():
             del a_dictionary[key]
         return a_dictionary.copy()
