4a3de1c90bcedcb907d71cca1e86055b95c91371
gotarazo <2272@holbertonschool.com>
2020-12-18 04:39:39 +0000

    Python more data structures weight average commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    scors = [score*weight for (score, weight) in my_list]
+    return sum(scors) / sum([weight for (score, weight) in my_list])#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    scors = [score*weight for (score, weight) in my_list]
+    return sum(scoes) / sum([weight for (score, weight) in my_list])
