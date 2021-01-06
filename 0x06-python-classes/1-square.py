9f3fd14b6f3894124fa2e02b249cc6cee42a05f0
gotarazo <2272@holbertonschool.com>
2021-01-06 02:42:56 +0000

    Python Classes & Objects commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Defines a square"""
 
 
 class Square:
     """Class square with private instance attribute: size"""
+
     def __init__(self, size):
         """Initialization function for the square class"""
+
         self.__size = size
