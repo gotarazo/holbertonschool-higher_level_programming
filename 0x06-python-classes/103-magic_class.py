4ee0032304afd6b56399a9ffd2497b277ceb7b1a
gotarazo <2272@holbertonschool.com>
2021-01-06 04:49:10 +0000

    Python Classes & Objects commit

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a Python class MagicClass doing exactly the same as a Python bytecode"""
+
+import math
+
+
+class MagicClass:
+    """Represent a Circle"""
+
+    def __init__(self, radius=0):
+        """Initialize the MagicClass"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Calculates the area of the Circle-MagicClass"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculates the circumference of the Circle-MagicClass"""
+        return (2 * math.pi * self.__radius)
