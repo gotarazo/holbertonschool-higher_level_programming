693b3621543f7138c9191e88beee76c9b0214123
gotarazo <2272@holbertonschool.com>
2021-01-08 04:16:25 +0000

    Python Test driven commit

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a matrix multiplication function (using NumPy)"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Calculates the multiplication of two matrix"""
+    return (np.matmul(m_a, m_b))
