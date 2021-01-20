606163047eae92d9cec3aff0f5350784dd761c7d
gotarazo <2272@holbertonschool.com>
2021-01-20 01:07:18 +0000

    Python Inheritance commit

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Defines the empty class BaseGeometry"""
 
 
 class BaseGeometry():
-    """Defines the empty class BaseGeometry"""
+    """Represent a empty BaseGeometry"""
     pass
