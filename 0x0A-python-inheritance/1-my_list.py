c3038d0854705c04420a83d3a93660f6a9bdbe2e
gotarazo <2272@holbertonschool.com>
2021-01-20 01:13:43 +0000

    Python Inheritance commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
+"""Defines the extended version of list"""
 
 
 class MyList(list):
-    """Defines a extended version of list"""
+    """Represent a extended version of MyList"""
 
     def print_sorted(self):
         """Print the list but sorted (ascending sort)"""
