d7cc3b7673671ce89c628bdb13d485491ca8f06a
gotarazo <2272@holbertonschool.com>
2021-01-21 02:36:47 +0000

    Python Inheritance commit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-    """Returns True if the object is exactly an instance of the specified class"""
+    """Returns True if object is exactly an instance of specified class"""
 
     if type(obj) == a_class:
         return True
