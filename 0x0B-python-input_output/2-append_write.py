bce3e99d22e53619abbf492f15570f79de600e43
gotarazo <2272@holbertonschool.com>
2021-01-21 03:10:08 +0000

    Python Input/Output commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string at the end of a UTF8 text file and return number of chars"""
+    """Appends string at end of text file and return number of chars"""
     with open(filename, "a", encoding="utf-8") as _file:
         return _file.write(text)
